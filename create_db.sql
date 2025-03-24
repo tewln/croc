25e554b348ee3cc47924ee26fe6e905589506272
tewln <161951015+tewln@users.noreply.github.com>
2025-03-24 17:47:54 +0500

    ver 3

--- create_db.sql
+++ create_db.sql
@@ -1,39 +1,38 @@
-CREATE SCHEMA IF NOT EXISTS CROC;
+CREATE SCHEMA IF NOT EXISTS croc;
 
-DROP TABLE IF EXISTS CROC.User CASCADE;
-DROP TABLE IF EXISTS CROC.Patient CASCADE;
-DROP TABLE IF EXISTS CROC.Anamnesis CASCADE;
-DROP TABLE IF EXISTS CROC.Diagnosis CASCADE;
-DROP TABLE IF EXISTS CROC.Measure_unit CASCADE;
-DROP TABLE IF EXISTS CROC.Preparation CASCADE;
-DROP TABLE IF EXISTS CROC.Measure CASCADE;
-DROP TABLE IF EXISTS CROC.Measure_book CASCADE;
-DROP TABLE IF EXISTS CROC.Preparation_book CASCADE;
-DROP TABLE IF EXISTS CROC.Organization CASCADE;
-DROP TABLE IF EXISTS CROC.Department CASCADE;
-DROP TABLE IF EXISTS CROC.Ward CASCADE;
-DROP TABLE IF EXISTS CROC.Staff CASCADE;
+DROP TABLE IF EXISTS croc."user" CASCADE;
+DROP TABLE IF EXISTS croc.patient CASCADE;
+DROP TABLE IF EXISTS croc.anamnesis CASCADE;
+DROP TABLE IF EXISTS croc.diagnosis CASCADE;
+DROP TABLE IF EXISTS croc.measure_unit CASCADE;
+DROP TABLE IF EXISTS croc.preparation CASCADE;
+DROP TABLE IF EXISTS croc.measure CASCADE;
+DROP TABLE IF EXISTS croc.measure_book CASCADE;
+DROP TABLE IF EXISTS croc.preparation_book CASCADE;
+DROP TABLE IF EXISTS croc.organization CASCADE;
+DROP TABLE IF EXISTS croc.department CASCADE;
+DROP TABLE IF EXISTS croc.ward CASCADE;
+DROP TABLE IF EXISTS croc.staff CASCADE;
 
-CREATE TABLE CROC.Organization (
+CREATE TABLE croc.organization (
   id SERIAL,
   name VARCHAR(200) NOT NULL,
 
   CONSTRAINT organization_id_pkey PRIMARY KEY (id)
 );
 
-CREATE TABLE CROC.Department (
+CREATE TABLE croc.department (
   id SERIAL,
   name VARCHAR(100) NOT NULL,
   pid INT NOT NULL,
   
   CONSTRAINT department_id_pkey PRIMARY KEY (id),
   CONSTRAINT department_pid_fkey FOREIGN KEY (pid)
-  REFERENCES CROC.Organization (id)
-  ON DELETE CASCADE
+  REFERENCES croc.organization (id)
   ON UPDATE CASCADE
 );
 
-CREATE TABLE CROC.Ward (
+CREATE TABLE croc.ward (
 --Палаты, приёмные и др. помещения
 
   id SERIAL,
@@ -44,37 +43,33 @@ CREATE TABLE CROC.Ward (
 
   CONSTRAINT ward_id_pkey PRIMARY KEY (id),
   CONSTRAINT ward_pid_fkey FOREIGN KEY (pid)
-  REFERENCES CROC.Department (id)
-  ON DELETE CASCADE
+  REFERENCES croc.department (id)
   ON UPDATE CASCADE
 );
 
-CREATE TABLE CROC.Staff (
+CREATE TABLE croc.staff (
   id SERIAL,
   surname VARCHAR(50) NOT NULL,
   firstname VARCHAR(50) NOT NULL,
   lastname VARCHAR(50),
-  position VARCHAR(100) NOT NULL,
+  "position" VARCHAR(100) NOT NULL,
   organization INT NOT NULL,
   department INT NOT NULL,
   ward INT,
   
   CONSTRAINT staff_id_pkey PRIMARY KEY (id),
   CONSTRAINT staff_organization_fkey FOREIGN KEY (organization)
-  REFERENCES CROC.Organization (id)
-  ON DELETE CASCADE
+  REFERENCES croc.organization (id)
   ON UPDATE CASCADE,
   CONSTRAINT staff_department_fkey FOREIGN KEY (department)
-  REFERENCES CROC.Department (id)
-  ON DELETE CASCADE
+  REFERENCES croc.department (id)
   ON UPDATE CASCADE,
   CONSTRAINT staff_ward_fkey FOREIGN KEY (ward)
-  REFERENCES CROC.Ward (id)
-  ON DELETE RESTRICT
-  ON UPDATE RESTRICT
+  REFERENCES croc.ward (id)
+  ON UPDATE CASCADE
 );
 
-CREATE TABLE CROC.Patient (
+CREATE TABLE croc.patient (
   id SERIAL,
   surname VARCHAR(50) NOT NULL,
   firstname VARCHAR(50) NOT NULL,
@@ -83,11 +78,11 @@ CREATE TABLE CROC.Patient (
   allergy VARCHAR(400),
 
   CONSTRAINT patient_id_pkey PRIMARY KEY (id),
-  CONSTRAINT valid_birth_date
+  CONSTRAINT paieint_birth_date_valid
   CHECK (birth_date <= CURRENT_TIMESTAMP)
 );
 
-CREATE TABLE CROC.Diagnosis (
+CREATE TABLE croc.diagnosis (
   id SERIAL,
   code VARCHAR(20) NOT NULL,
   name VARCHAR(100) NOT NULL,
@@ -97,7 +92,7 @@ CREATE TABLE CROC.Diagnosis (
   UNIQUE (code, name)
 );
 
-CREATE TABLE CROC.Measure_unit (
+CREATE TABLE croc.measure_unit (
 --Единица измерения, в которой измеряется лекарство, и его форма
 
   id SERIAL,
@@ -107,12 +102,12 @@ CREATE TABLE CROC.Measure_unit (
   CONSTRAINT measure_unit_id_pkey PRIMARY KEY (id)
 );
 
-CREATE TABLE CROC.Preparation (
+CREATE TABLE croc.preparation (
 --Препараты, находящиеся в ходу
   id SERIAL,
   name VARCHAR(100) NOT NULL,
   form INT NOT NULL,
-  dosage INT NOT NULL,
+  dosage SMALLINT NOT NULL,
   release_date DATE NOT NULL,
   expiration_date DATE NOT NULL,
   manufacturer VARCHAR(100) NOT NULL,
@@ -120,20 +115,15 @@ CREATE TABLE CROC.Preparation (
 
   CONSTRAINT preparation_id_pkey PRIMARY KEY (id),
   CONSTRAINT preparation_form_fkey FOREIGN KEY (form)
-  REFERENCES CROC.Measure_unit (id)
-  ON DELETE CASCADE
+  REFERENCES croc.measure_unit (id)
   ON UPDATE CASCADE,
-  CONSTRAINT preparation_dosage_fkey FOREIGN KEY (dosage)
-  REFERENCES CROC.Measure_unit (id)
-  ON DELETE CASCADE
-  ON UPDATE CASCADE,
-  CONSTRAINT preparation_valid_date
+  CONSTRAINT preparation_date_valid
   CHECK (release_date <= CURRENT_DATE AND release_date <= expiration_date),
-  CONSTRAINT preparation_valid_int
+  CONSTRAINT preparation_int_valid
   CHECK (dosage > 0)
 );
 
-CREATE TABLE CROC.Preparation_book (
+CREATE TABLE croc.preparation_book (
 --Журнал выдачи препаратов
   id SERIAL,
   patient INT NOT NULL,
@@ -143,18 +133,16 @@ CREATE TABLE CROC.Preparation_book (
 
   CONSTRAINT preparation_book_id_pkey PRIMARY KEY (id),
   CONSTRAINT preparation_book_patient_fkey FOREIGN KEY (patient)
-  REFERENCES CROC.Patient (id)
-  ON DELETE CASCADE
+  REFERENCES croc.patient (id)
   ON UPDATE RESTRICT,
   CONSTRAINT prepraration_book_preparation_fkey FOREIGN KEY (preparation)
-  REFERENCES CROC.Preparation (id)
-  ON DELETE CASCADE
+  REFERENCES croc.preparation (id)
   ON UPDATE CASCADE,
-  CONSTRAINT preparation_book_valid_int
+  CONSTRAINT preparation_book_int_valid
   CHECK (quantity > 0)
 );
 
-CREATE TABLE CROC.Measure (
+CREATE TABLE croc.measure (
 --Проводимые измерения
  id SERIAL,
  name VARCHAR(100) NOT NULL,
@@ -163,25 +151,23 @@ CREATE TABLE CROC.Measure (
  CONSTRAINT measure_id_pkey PRIMARY KEY (id)
 );
 
-CREATE TABLE CROC.Measure_book (
+CREATE TABLE croc.measure_book (
 --Журнал проведенных измерений
   id SERIAL,
-  measure_type INT NOT NULL,
   patient INT NOT NULL,
+  measure_type INT NOT NULL,
   date TIMESTAMP NOT NULL,
 
   CONSTRAINT measure_book_id_pkey PRIMARY KEY (id),
-  CONSTRAINT measure_book_measure_fkey FOREIGN KEY (measure_type)
-  REFERENCES CROC.Measure (id)
-  ON DELETE CASCADE
-  ON UPDATE CASCADE,
   CONSTRAINT measure_book_patient_fkey FOREIGN KEY (patient)
-  REFERENCES CROC.Patient (id)
-  ON DELETE CASCADE
-  ON UPDATE RESTRICT
+  REFERENCES croc.patient (id)
+  ON UPDATE RESTRICT,
+  CONSTRAINT measure_book_measure_fkey FOREIGN KEY (measure_type)
+  REFERENCES croc.measure (id)
+  ON UPDATE CASCADE
 );
 
-CREATE TABLE CROC.Anamnesis (
+CREATE TABLE croc.anamnesis (
   id BIGSERIAL,
   department INT NOT NULL,
   ward INT NOT NULL,
@@ -192,51 +178,245 @@ CREATE TABLE CROC.Anamnesis (
   mobility BOOLEAN NOT NULL,
   doctor INT,
   nurse INT,
-  measure INT,
-  preparation INT,
+  measure INT, /**/
+  preparation INT, /**/
 
   CONSTRAINT anamnesis_id_pkey PRIMARY KEY (id),
   CONSTRAINT anamnesis_department_fkey FOREIGN KEY (department)
-  REFERENCES CROC.Department (id)
-  ON DELETE SET DEFAULT /**/
+  REFERENCES croc.department (id)
   ON UPDATE RESTRICT,
   CONSTRAINT anamnesis_ward_fkey FOREIGN KEY (ward)
-  REFERENCES CROC.Ward (id)
-  ON DELETE SET DEFAULT /**/
+  REFERENCES croc.ward (id)
   ON UPDATE RESTRICT,
   CONSTRAINT anamnesis_patient_fkey FOREIGN KEY (patient)
-  REFERENCES CROC.Patient (id)
-  ON DELETE RESTRICT
-  ON UPDATE RESTRICT,
+  REFERENCES croc.patient (id)
+  ON UPDATE CASCADE,
+  CONSTRAINT anamnesis_date_valid
+  CHECK (admission_date <= discharge_date AND admission_date <= CURRENT_TIMESTAMP),
   CONSTRAINT anamnesis_diagnosis_fkey FOREIGN KEY (diagnosis)
-  REFERENCES CROC.Diagnosis (id)
-  ON DELETE RESTRICT
+  REFERENCES croc.diagnosis (id)
   ON UPDATE CASCADE,
   CONSTRAINT anamnesis_doctor_fkey FOREIGN KEY (doctor)
-  REFERENCES CROC.Staff (id)
-  ON DELETE RESTRICT
-  ON UPDATE CASCADE,
+  REFERENCES croc.staff (id)
+  ON UPDATE RESTRICT,
   CONSTRAINT anamnesis_nurse_fkey FOREIGN KEY (nurse)
-  REFERENCES CROC.Staff (id)
-  ON DELETE RESTRICT
-  ON UPDATE CASCADE,
+  REFERENCES croc.staff (id)
+  ON UPDATE RESTRICT,
   CONSTRAINT anamnesis_measure_fkey FOREIGN KEY (measure)
-  REFERENCES CROC.Measure_book (id)
-  ON DELETE SET DEFAULT /**/
+  REFERENCES croc.measure_book (id)
   ON UPDATE CASCADE,
   CONSTRAINT anamnesis_preparation_fkey FOREIGN KEY (preparation)
-  REFERENCES CROC.Preparation_book (id)
-  ON DELETE SET DEFAULT /**/
+  REFERENCES croc.preparation_book (id)
   ON UPDATE CASCADE
 );
 
-CREATE TABLE CROC.User (
-  id SERIAL,
-  login VARCHAR(200) /*UNIQUE*/,
-  password VARCHAR(200),
-  staff INT /*UNIQUE*/,
+CREATE TABLE croc."user" (
+  id SERIAL NOT NULL,
+  login VARCHAR(200) NOT NULL,
+  password VARCHAR(200) NOT NULL,
+  staff INT NOT NULL,
 
-  CONSTRAINT id_pkey PRIMARY KEY (id),
-  CONSTRAINT staff_fkey FOREIGN KEY (staff)
-  REFERENCES CROC.Staff (id)
+  CONSTRAINT user_id_pkey PRIMARY KEY (id),
+  CONSTRAINT user_login_uniq UNIQUE(login),
+  CONSTRAINT user_staff_fkey FOREIGN KEY (staff)
+  REFERENCES croc.staff (id),
+  CONSTRAINT user_staff_uniq UNIQUE (staff)
 );
+
+
+
+
+COMMENT ON TABLE croc.anamnesis
+    IS 'Информация об итерациях нахождения в стационаре пациентов';
+
+COMMENT ON COLUMN croc.anamnesis.department
+    IS 'Отделение, в котором находится пациент во время итерации';
+
+COMMENT ON COLUMN croc.anamnesis.ward
+    IS 'Палата, в которой располагается пациент во время итерации';
+
+COMMENT ON COLUMN croc.anamnesis.patient
+    IS 'Описываемый пациент';
+
+COMMENT ON COLUMN croc.anamnesis.admission_date
+    IS 'Время поступления (начала итерации)';
+
+COMMENT ON COLUMN croc.anamnesis.discharge_date
+    IS 'Время выписки (окончания итерации)';
+
+COMMENT ON COLUMN croc.anamnesis.diagnosis
+    IS 'Диагноз, поставленный в этой итерации';
+
+COMMENT ON COLUMN croc.anamnesis.mobility
+    IS 'Мобильность пациента в итерации';
+
+COMMENT ON COLUMN croc.anamnesis.doctor
+    IS 'Доктор, за которым закреплён пациент';
+
+COMMENT ON COLUMN croc.anamnesis.nurse
+    IS 'Медсестра, за которой закреплён пациент';
+
+COMMENT ON COLUMN croc.anamnesis.measure
+    IS 'Проведённые и предстоящие измерения у пациента';
+
+COMMENT ON COLUMN croc.anamnesis.preparation
+    IS 'Назначенные и выданные препараты';
+	
+COMMENT ON TABLE croc.department
+    IS 'Отделения мед.учреждений';
+
+COMMENT ON COLUMN croc.department.name
+    IS 'Название отделения';
+
+COMMENT ON COLUMN croc.department.pid
+    IS 'Мед.учреждение, к которому относится отделение';
+	
+COMMENT ON TABLE croc.diagnosis
+    IS 'Список возможных диагнозов ';
+
+COMMENT ON COLUMN croc.diagnosis.code
+    IS 'Код диагноза МКБ';
+
+COMMENT ON COLUMN croc.diagnosis.name
+    IS 'Название диагноза МКБ';
+
+COMMENT ON TABLE croc.measure
+    IS 'Список возможных измерений у пациента';
+
+COMMENT ON COLUMN croc.measure.name
+    IS 'Название измерения';
+
+COMMENT ON COLUMN croc.measure.measure_unit
+    IS 'Мера, в которой измеряется';
+
+COMMENT ON TABLE croc.measure_book
+    IS 'Проводимые измерения у пациентов';
+
+COMMENT ON COLUMN croc.measure_book.measure_type
+    IS 'Какое измерение проводится';
+
+COMMENT ON COLUMN croc.measure_book.patient
+    IS 'Пациент, у которого берётся измерение';
+
+COMMENT ON COLUMN croc.measure_book.date
+    IS 'Дата и время, назначенные для проведения измерения';
+
+COMMENT ON TABLE croc.measure_unit
+    IS 'Список мер измерения количества лекарственных средств';
+
+COMMENT ON COLUMN croc.measure_unit.measure_object
+    IS 'Измеряемый параметр';
+
+COMMENT ON COLUMN croc.measure_unit.measure_unit
+    IS 'Мера измерения параметра';
+	
+COMMENT ON TABLE croc.organization
+    IS 'Мед.учреждения, подключенные к системе';
+
+COMMENT ON COLUMN croc.organization.name
+    IS 'Название мед.учреждения';
+
+COMMENT ON TABLE croc.patient
+    IS 'Пациенты, посетившие мед.учреждения';
+
+COMMENT ON COLUMN croc.patient.surname
+    IS 'Фамилия пациента';
+
+COMMENT ON COLUMN croc.patient.firstname
+    IS 'Имя пациента';
+
+COMMENT ON COLUMN croc.patient.lastname
+    IS 'Отчество пациента';
+
+COMMENT ON COLUMN croc.patient.birth_date
+    IS 'День рождения пациента';
+
+COMMENT ON COLUMN croc.patient.allergy
+    IS 'Аллергии, заявленные/выявленные пациентом (у пациента)';
+
+COMMENT ON TABLE croc.preparation
+    IS 'Препараты, находящиеся в распоряжении мед.учреждений';
+
+COMMENT ON COLUMN croc.preparation.name
+    IS 'Название препарата';
+
+COMMENT ON COLUMN croc.preparation.form
+    IS 'Форма выпуска';
+
+COMMENT ON COLUMN croc.preparation.dosage
+    IS 'Дозировка в единице препарата';
+
+COMMENT ON COLUMN croc.preparation.release_date
+    IS 'Дата выпуска партии';
+
+COMMENT ON COLUMN croc.preparation.expiration_date
+    IS 'Дата истечения срока годности партии';
+
+COMMENT ON COLUMN croc.preparation.manufacturer
+    IS 'Производитель препарата';
+
+COMMENT ON COLUMN croc.preparation.narcotic
+    IS 'Является наркотическим средством?';
+	
+COMMENT ON TABLE croc.preparation_book
+    IS 'Препараты, назначенные пациентам';
+
+COMMENT ON COLUMN croc.preparation_book.patient
+    IS 'Какому пациенту выдан препарат';
+
+COMMENT ON COLUMN croc.preparation_book.preparation
+    IS 'Какой препарат выписан';
+
+COMMENT ON COLUMN croc.preparation_book.quantity
+    IS 'Количество выписанного препарата';
+
+COMMENT ON COLUMN croc.preparation_book.date
+    IS 'Дата и время назначенного приёма';
+
+COMMENT ON TABLE croc.staff
+    IS 'Сотрудники мед.учреждений';
+
+COMMENT ON COLUMN croc.staff.surname
+    IS 'Фамилия сотрудника';
+
+COMMENT ON COLUMN croc.staff.firstname
+    IS 'Имя сотрудника';
+
+COMMENT ON COLUMN croc.staff.lastname
+    IS 'Отчество сотрудника';
+
+COMMENT ON COLUMN croc.staff."position"
+    IS 'Должность сотрудника';
+
+COMMENT ON COLUMN croc.staff.organization
+    IS 'Организация, в которой работает сотрудник';
+
+COMMENT ON COLUMN croc.staff.department
+    IS 'Отделения, в которой работает сотрудник';
+
+COMMENT ON COLUMN croc.staff.ward
+    IS 'Палата, в которой работает сотрудник';
+
+COMMENT ON TABLE croc."user"
+    IS 'Список авторизационных данных';
+
+COMMENT ON COLUMN croc."user".login
+    IS 'Логин сотрудника';
+
+COMMENT ON COLUMN croc."user".password
+    IS 'Пароль сотрудника';
+
+COMMENT ON COLUMN croc."user".staff
+    IS 'Чьи авторизационные данные?';
+
+COMMENT ON TABLE croc.ward
+    IS 'Палаты в отделениях';
+
+COMMENT ON COLUMN croc.ward.name
+    IS 'Название/номер палаты';
+
+COMMENT ON COLUMN croc.ward.pid
+    IS 'Отделение, к которой относится палата';
+
+COMMENT ON COLUMN croc.ward.activity
+    IS 'Работает ли палата?';
